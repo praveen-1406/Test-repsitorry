ee271a7bb8b25843631435e4ff24eb35bb4ac49d
praveen-1406 <prajapatipraveen1406@gmail.com>
2023-03-11 12:23:50 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,5 @@
+console.log("hello Javascript");
+console.log(document);
+console.log(document.body);
+console.log(document.body.style.backgroundolor="black");
+console.log(document.getElementById('para01').innerHTML);
